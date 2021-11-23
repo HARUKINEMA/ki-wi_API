0e1abdb00c96ef4344c4f4d7d2c1e1c147795ea8
Soya Uehara <e195752@ie.u-ryukyu.ac.jp>
2021-11-23 13:34:46 +0900

    POSTでリクエスト投げて、jsonに書き込める(未完成)

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import express from 'express'
+import * as fs from 'fs'
+
 
 const app: express.Express = express()
 const router: express.Router = express.Router()
 
-router.get('/api/hello', (req:express.Request, res:express.Response) => {
-  res.send("HelloWorld" + req.query.lat +" "+ req.query.lng)
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+router.get('/hello', (req:express.Request, res:express.Response) => {
+  res.send("HelloWorld" + req.query.lat +" "+ req.query.lng+ '\n')
 })
 
-app.use(router)
+router.post('/machine', (req:express.Request, res:express.Response) => {
+  //const a = { "lat": req.query.lat, "lng": req.query.lng }
+  const json_machine = JSON.stringify({ "lat": req.query.lat, "lng": req.query.lng });
+  res.send(json_machine);
+   fs.writeFileSync('./src/drinking_machine.json',json_machine)
+})
+
+app.use("/api", router)
 
 // 3000番ポートでAPIサーバ起動
 app.listen(3000,()=>{ console.log('Listening on port 3000...') })
