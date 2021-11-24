ef4131b1c957893b3f35f3c1e0a7cd4219c2e75b
Soya Uehara <e195752@ie.u-ryukyu.ac.jp>
2021-11-24 10:35:27 +0900

    位置情報を投げて,jsonで保存できる

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,37 @@
-import express from 'express'
-import * as fs from 'fs'
+import express from "express";
+import * as fs from "fs";
+import * as data from "./drinking_machine.json";
 
+const app: express.Express = express();
+const router: express.Router = express.Router();
+const listData: { lat: number; lng: number; }[] = [];
 
-const app: express.Express = express()
-const router: express.Router = express.Router()
+for (let key in data.machines){
+  listData.push(data.machines[key]);
+}
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-router.get('/hello', (req:express.Request, res:express.Response) => {
-  res.send("HelloWorld" + req.query.lat +" "+ req.query.lng+ '\n')
-})
+router.get("/hello", (req: express.Request, res: express.Response) => {
+  res.send("HelloWorld" + req.query.lat + " " + req.query.lng + "\n");
+});
 
-router.post('/machine', (req:express.Request, res:express.Response) => {
-  //const a = { "lat": req.query.lat, "lng": req.query.lng }
-  const json_machine = JSON.stringify({ "lat": req.query.lat, "lng": req.query.lng });
-  res.send(json_machine);
-   fs.writeFileSync('./src/drinking_machine.json',json_machine)
-})
+router.post("/machine", (req: express.Request, res: express.Response) => {
+  const reqDataJson = {
+    lat: Number(req.query.lat),
+    lng: Number(req.query.lng)
+  };
+  res.send(reqDataJson);
+  //result.jsonObject = reqDataJson;
+  listData.push(reqDataJson);
+  const machineData = JSON.stringify({"machines": listData}, null, ' ')
+  fs.writeFileSync('./lib/drinking_machine.json', machineData);
+});
 
-app.use("/api", router)
+app.use("/api", router);
 
 // 3000番ポートでAPIサーバ起動
-app.listen(3000,()=>{ console.log('Listening on port 3000...') })
+app.listen(3000, () => {
+  console.log("Listening on port 3000...");
+});
