c94cd4af96403eb7140c9c7e419dd650aa5e2e07
Soya Uehara <e195752@ie.u-ryukyu.ac.jp>
2021-12-21 17:58:08 +0900

    コードを整形

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,9 @@ import https from "https";
 import { exit } from "process";
 import * as dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 const ENV = process.env.REACT_ENV as string;
-console.log(ENV)
-
-var corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+console.log(ENV);
 
 const app: express.Express = express();
 const router: express.Router = express.Router();
@@ -26,55 +21,61 @@ for (const key in data.machines) {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-router.post("/api/machine", cors(), (req: express.Request, res: express.Response) => {
-  const reqData = {
-    location: [Number(req.query.lat), Number(req.query.lng)],
-  };
-  res.send(reqData);
-  dataList.push(reqData);
-  const machineData = JSON.stringify({ machines: dataList }, null, " ");
-  fs.writeFileSync("./lib/drinking_machine.json", machineData);
-});
+router.post(
+  "/api/machine",
+  cors(),
+  (req: express.Request, res: express.Response) => {
+    const reqData = {
+      location: [Number(req.query.lat), Number(req.query.lng)],
+    };
+    res.send(reqData);
+    dataList.push(reqData);
+    const machineData = JSON.stringify({ machines: dataList }, null, " ");
+    fs.writeFileSync("./lib/drinking_machine.json", machineData);
+  }
+);
 
 app.use("", router);
 
-if (ENV=="PRODUCT"){
-  const privateKey = "/home/ubuntu/private.key"
-  const certKey = "/home/ubuntu/certificate.crt"
-  const ca = "/home/ubuntu/ca_bundle.crt"
+if (ENV == "PRODUCT") {
+  const privateKey = "/home/ubuntu/private.key";
+  const certKey = "/home/ubuntu/certificate.crt";
+  const ca = "/home/ubuntu/ca_bundle.crt";
 
-  fs.open(privateKey, "r", (err,fd)=>{
-    if(err){
+  fs.open(privateKey, "r", (err) => {
+    if (err) {
       console.log("private-key error");
-      exit(1)
+      exit(1);
     }
-  })
+  });
 
-  fs.open(certKey, "r", (err,fd)=>{
-    if(err){
+  fs.open(certKey, "r", (err) => {
+    if (err) {
       console.log("private-key error");
-      exit(1)
+      exit(1);
     }
-  })
+  });
 
-  fs.open(ca, "r", (err,fd)=>{
-    if(err){
+  fs.open(ca, "r", (err) => {
+    if (err) {
       console.log("private-key error");
-      exit(1)
+      exit(1);
     }
-  })
-  
-  const server = https.createServer({
-    key: fs.readFileSync(privateKey),
-    cert: fs.readFileSync(certKey),
-    ca:fs.readFileSync(ca),
-  }, app)
+  });
+
+  const server = https.createServer(
+    {
+      key: fs.readFileSync(privateKey),
+      cert: fs.readFileSync(certKey),
+      ca: fs.readFileSync(ca),
+    },
+    app
+  );
   server.listen(443, () => {
-    console.log("start")
-  })
-}else{
-  app.listen(8080, ()=>{
-    console.log("test server start")
-  })
+    console.log("start");
+  });
+} else {
+  app.listen(8080, () => {
+    console.log("test server start");
+  });
 }
-
