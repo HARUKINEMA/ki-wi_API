4ca00a6d65a2a6dd00f0952b8cd08b52c58a4b03
SC-Bee <e195743@u-ryukyu.ac.jp>
2021-12-24 14:20:19 +0900

    Png2Jpg機能

--- src/png2jpg.ts
+++ src/png2jpg.ts
@@ -1,11 +1,25 @@
 import Jimp from "Jimp";
+import { v4 as uuidv4 } from 'uuid';
+
 
 export function Png2Jpg(filepath:string){
     Jimp.read(filepath, function (err, lenna) {
         if (err) throw err;
-        lenna.resize(256, 256)            // resize
-             .write("lena-small-bw.jpg"); // save
+        lenna.resize(3000, 4000)      // resize
+             .quality(85)             // quality
+             .write(uuidv4()+".jpg"); // save
     });    
 }
+
+export function Jpg2Jpg(filepath:string){
+    Jimp.read(filepath, function (err, lenna) {
+        if (err) throw err;
+        lenna.resize(3000, 4000)      // resize
+             .quality(85)             // quality
+             .write(uuidv4()+".jpg"); // save
+    });    
+}
+
+
 
 Png2Jpg("a.png");
